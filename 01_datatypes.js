8a14d86198a86a5c95c3fb9f233deb234e8fda88
manpreetpahasp <preetkaur@pahasp.com>
2026-01-10 16:17:16 +0530

    all_datatypes

---
+++ 01_datatypes.js
@@ -0,0 +1,27 @@
+"Use strict" //treat all js code as newer version
+
+// alert(3+3) // we rae using nodejs,not browser
+
+console.log( 3 + 3 )
+console.log("Manpreet")
+
+let name = "Mapreet"
+let age = 23
+let isLoggedIN = false
+let state;
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefined); //
--- 01_variables.js
+++ 01_variables.js
@@ -2,3 +2,19 @@ const account_ID = 144562
 let accountEmail = "reet@gmailcom"
 var accountPassword = "12345"
 accountCity = "Jalandhar"
+let accountState;
+
+// account_ID = 3// Not Allowed means const does not change
+
+accountEmail = "manpreet@gmail.com"
+accountPassword = "09876"
+accountCity = "Delhi"
+
+console.log(account_ID);
+
+/*
+Prefer not to use var 
+because of issue in block scope and funtional scope
+*/
+
+console.table([account_ID,accountEmail,accountPassword,accountCity,accountState]);
