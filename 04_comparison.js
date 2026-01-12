97fa3600d0f412290daddacaa2d1f3fcdeddaa0e
manpreetpahasp <preetkaur@pahasp.com>
2026-01-12 11:13:51 +0530

    first commit

---
+++ 04_comparison.js
@@ -0,0 +1,23 @@
+// Basic comparison operations in javascript
+
+// console.log(2 >1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//**** When we compare the different datatypes
+console.log("2" > 1);
+
+// when we compare null 
+console.log (null > 0 );
+console.log (null == 0 );
+console.log (null >= 0 );
+
+//when we compare undefined
+console.log (undefined> 0 );
+console.log (undefined == 0 );
+console.log (undefined >= 0 );
+
+// ===(strict check means also checks the value of the dattype)
+console.log("2" === 2);
