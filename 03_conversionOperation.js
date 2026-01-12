ddb6f52b5b6df5dcced154b59778f4632b856fda
manpreetpahasp <preetkaur@pahasp.com>
2026-01-12 09:44:50 +0530

    error_solve

---
+++ 03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "abc12"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+//"abc12" => NaN
+// true => 1; false => 0
+ 
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
